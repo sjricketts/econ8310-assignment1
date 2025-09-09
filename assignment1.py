0c5ad64fd3b9307234d1035a52074b6501e15fa8
Scott Ricketts <34109522+sjricketts@users.noreply.github.com>
2025-09-08 19:50:49 -0500

    fitting exponential smoothing model

--- assignment1.py
+++ assignment1.py
@@ -1,2 +1,15 @@
+# libraries
 from statsmodels.tsa.api import ExponentialSmoothing
 import pandas as pd
+
+# import data
+data = pd.read_csv("https://raw.githubusercontent.com/dustywhite7/econ8310-assignment1/main/assignment_data_train.csv")
+
+# dependant variable
+model = data['trips']
+
+# model fit with seasonality (not dampened)
+modelFit = ExponentialSmoothing(model, 
+                                trend='add' ,
+                                seasonal='add', 
+                                seasonal_periods=12).fit()
