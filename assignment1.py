4b2352f081bba47974bb0c7bb0dfec51e8ad99ba
Scott Ricketts <34109522+sjricketts@users.noreply.github.com>
2025-09-13 17:43:05 -0500

    prophet attempt; chngpt0.5

--- assignment1.py
+++ assignment1.py
@@ -1,21 +1,25 @@
-# libraries
-from statsmodels.tsa.api import ExponentialSmoothing
+# Import statements
 import pandas as pd
+import numpy as np
+from prophet import Prophet
 
 # import data
 data = pd.read_csv("https://raw.githubusercontent.com/dustywhite7/econ8310-assignment1/main/assignment_data_train.csv")
 
-# dependant variable
-trips = data['trips']
+# grab the two neede columns
+trips_data = data[['Timestamp','trips']]
+# convert to datetime
+trips_data['Timestamp'] = pd.to_datetime(trips_data["Timestamp"])
+# Recreate the data frame with correct labels
+trips_data = pd.DataFrame(trips_data.values, columns = ['ds','y'])
 
-# valid model
-model = ExponentialSmoothing(trips,
-                            trend='add' ,
-                            seasonal='add', 
-                            seasonal_periods=12)
+# Initialize Prophet instance and fit to data
+model = Prophet(changepoint_prior_scale=0.5)
+modelFit = model.fit(trips_data)
 
-# model fit
-modelFit = model.fit()
+# Create timeline for 744 hours in future
+# documentation: https://facebook.github.io/prophet/docs/non-daily_data.html#sub-daily-data  
+future = modelFit.make_future_dataframe(periods=744,freq='H')
 
-#predict
-pred = modelFit.forecast(744)
+# Generate predictions based on that timeline
+pred = modelFit.predict(future)
