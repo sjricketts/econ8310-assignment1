8c7476a7c65a2627ec7faf873c28d2d5686f1662
Scott Ricketts <34109522+sjricketts@users.noreply.github.com>
2025-09-17 14:32:08 -0500

    daily seasonality

--- assignment1.py
+++ assignment1.py
@@ -16,10 +16,11 @@ trips_data['Timestamp'] = pd.to_datetime(trips_data["Timestamp"])
 trips_data = pd.DataFrame(trips_data.values, columns = ['ds','y'])
 
 
-# Initialize Prophet instance; seasonality mode; add weekly seasonality
+# Initialize Prophet instance; seasonality mode; add weekly/daily seasonality
 model = Prophet(changepoint_prior_scale=0.05,
                 seasonality_mode='additive',
-                weekly_seasonality=True)
+               weekly_seasonality=True,
+               daily_seasonality =True)
 
 # fit to data
 modelFit = model.fit(trips_data)
